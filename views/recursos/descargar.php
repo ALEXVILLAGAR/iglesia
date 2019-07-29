26ac330fedb279edecb83e9b868804dff7003f08
ALEXVILLAGAR <alexvilla@utp.edu.co>
2019-07-29 16:41:55 -0500

    9 cambio de problemas notados en el servidor

--- views/recursos/descargar.php
+++ views/recursos/descargar.php
@@ -1,7 +1,7 @@
 <?php 
 $archivo = $_POST['nombre'];
 // $file = file("C:/xampp/htdocs/iglesia/public/recursos/recursos1/$archivo");
-$file = file("../public/recursos/recursos1/$archivo");
+$file = file("/var/www/html/iglesia/public/recursos/recursos1/$archivo");
 $file2 = implode ("", $file);
 header("Content-type: application/force-download"); 
 header("Content-Disposition: attachment; filename=$archivo"); 
