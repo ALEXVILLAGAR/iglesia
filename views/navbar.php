38fd1a196423e2f413e491fa0cc53650c1e9b55b
ALEXVILLAGAR <alexvilla@utp.edu.co>
2019-07-30 18:04:32 -0500

    mejorando logo responsive

--- views/navbar.php
+++ views/navbar.php
@@ -3,7 +3,7 @@
       <button class="navbar-toggler navbar-toggler-right " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="menu">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#"><img src="public/img/logoIglesia/logo.png" id="inicio1" class="logomenu"></a>
+      <a class="navbar-brand" href="#"><img src="public/img/logoIglesia/logo.png" id="inicio1" width="230"></a>
 
       <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav ">
