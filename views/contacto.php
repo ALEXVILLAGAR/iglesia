3ee09b99bd9b8f105715a0dee7cb319f65c63ba6
ALEXVILLAGAR <alexvilla@utp.edu.co>
2019-07-30 19:39:23 -0500

    mejorando responsive de contacto

--- views/contacto.php
+++ views/contacto.php
@@ -1,10 +1,14 @@
-<div class="row bajar ocultar" id="mi-contacto">
+<div class="row bajar1 ocultar" id="mi-contacto">
 
-	<h3 class="col-md-12 text-center ">Contacto</h3>
-	<hr class="col-md-10 col-offset-0">
-	<br>
+    <div class="col-md-12 col-offset-0">
+	    <h3 class="col-md-12 text-center ">Contacto</h3>
+		<hr class="col-md-10 col-offset-1">
+		<br>
+    </div>
+	
+
 	<div class="row text-center">
-		<div class=" text-white bg-secondary mb-3  col-md-4 ml-8" style="max-width: 18rem;">
+		<div class=" text-white bg-secondary mb-3  col-md-3 ml-9 mt-3 " style="max-width: 18rem;">
 		  <div class="card-header bg-primary2">
 		  	<h5 class="text-center ml-4"> Reunión Abuelos Felices</h5></div>
 		  <div class="card-body p-2">
@@ -17,7 +21,7 @@
 		  </div>
 		</div>
 
-		<div class=" text-white bg-secondary mb-3  col-md-4  ml-8" style="max-width: 18rem;">
+		<div class=" text-white bg-secondary mb-3  col-md-3 ml-9 mt-3" style="max-width: 18rem;">
 		  <div class="card-header bg-primary2">
 		  	<h5 class="text-center ml-4 mt-3"> Reuniones generales</h5></div>
 		  <div class="card-body p-2">
@@ -35,7 +39,7 @@
 		  </div>
 		</div>
 
-		<div class=" text-white bg-secondary mb-3  col-md-4 ml-8" style="max-width: 18rem;">
+		<div class=" text-white bg-secondary mb-3  col-md-3 offset-1 mt-3" style="max-width: 18rem;">
 		  <div class="card-header bg-primary2">
 		  	<h5 class="text-center ml-4 mt-3"> Horarios de oficina
              </h5>
@@ -54,10 +58,12 @@
 		  </div>
 		</div>
          
-		<div class=" col-md-10 bg-secondary text-white ml-9 mt-5">
+		<div class=" col-md-10 bg-secondary text-white offset-1 mt-5">
   <div class=" bg-secondary">
     <h5>Domicilio</h5>
-    <hr class="col-md-1">
+    <div class="col-md-12">
+    	<hr class="col-md-1">
+    </div>
   </div>
   <div class=" m-4">
     <p class="">CRA 16 N° 18-40, Santa Rosa de Cabal, Risaralda.
@@ -67,7 +73,9 @@
    </div>
    <div> 
    	<h6>nuestras redes</h6>
-   	<hr class="col-md-1">
+   	<div class="col-md-12">
+   		<hr class="col-md-1">
+   	</div>
    	<a href="https://www.youtube.com/channel/UCHArXYSb6l9-J6sCYOjDHAQ?view_as=subscriber" title="" target="blank"><i class="fab fa-youtube fa-2x text-danger"></i></a>
    <a href="https://www.facebook.com/IICCMD/" title="" target="blank" class="ml-2"><i class="fab fa-facebook-square fa-2x text-info"></i></a>
    <br>
