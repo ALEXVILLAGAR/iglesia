cf199fc55ba68147c41b6b0b9d2b5a8bb3c0ddaf
ALEXVILLAGAR <alexvilla@utp.edu.co>
2019-07-30 20:50:35 -0500

    agregando icono de la pestaña

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <link href="public/img/logoIglesia/logo2.jpg" rel="shortcut icon"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
      
  <!-- Fonts -->
